7e1f4e7220714da3e5ec8d84695a275851cb8ed7
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-04-26 07:31:37 +0000

    Time: 50 ms (87.83%) | Memory: 51.3 MB (44.56%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.ts
@@ -0,0 +1,20 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    
+    let count = 0;
+    return function (...args) {
+       if(count > 0) return undefined
+        count++
+        return fn(...args)
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
