45400a9c7e1912508665d9338d3a3bfc7ae11514
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-04-30 08:38:14 +0000

    Time: 128 ms (57.89%), Space: 57.3 MB (53.51%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.ts
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.ts
@@ -1,10 +1,9 @@
 function rangeBitwiseAnd(left: number, right: number): number {
-    let shift = 0
-    // Find the common prefix
-    while (left < right) {
-        left >>= 1
-        right >>= 1
-        ++shift
-    }
-    return left << shift
+    // Calculate the difference in binary length between 'right' and 'left'
+    // If 'left' and 'right' are the same, the difference is 0
+    const diffLength = left === right ? 0 : (right - left).toString(2).length;
+    // Shift 'left' and 'right' to the right by 'diffLength' bits, then shift them back to the left
+    // This effectively zeroes out the last 'diffLength' bits of 'left' and 'right'
+    // The bitwise AND of the resulting numbers is the same as the bitwise AND of all numbers in the range
+    return (left >> diffLength << diffLength) & (right >> diffLength << diffLength);
 };
