729332002a6f0d0e92e517d46b9abeb8ab654eda
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-05-05 09:16:52 +0000

    Time: 83 ms (39.38%), Space: 53.6 MB (76.06%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.ts
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.ts
@@ -1,27 +1,30 @@
-function maximumOddBinaryNumber(binaryString: string): string {
+// This function takes a binary string as input and returns the maximum odd binary number that can be formed using the same number of '1's in the input string.
+function maximumOddBinaryNumber(s: string): string {
+    // Get the length of the input string.
+    let len = s.length, cnt = 0
 
-    // Initialize count to keep track of the number of '1's in the binary string.
-
-    let onesCount = 0;
-
-
-    // Iterate through each character of the binary string input.
-
-    for (const character of binaryString) {
-
-        // Increment the count for every '1' found.
-
-        onesCount += character === '1' ? 1 : 0;
-
+    // Count the number of '1's in the input string.
+    for (let i = 0; i < len; i++) {
+        if (s[i] === '1') {
+            cnt++
+        }
     }
 
+    // Initialize the answer string.
+    let ans = ""
 
-    // Generate the maximum odd binary number by repeating '1' for (onesCount - 1) times,
+    // Add '1's to the answer string. The number of '1's is one less than the count of '1's in the input string.
+    for (let i = 0; i < cnt - 1; i++) {
+        ans += '1'
+    }
 
-    // followed by repeating '0' for (string length - onesCount) times, and then appending '1' at the end of the string.
+    for (let i = 0; i < len - cnt; i++) {
+        ans += '0'
+    }
 
-    // This maximizes the number of '1' digits at the front, while ensuring the last digit is '1' for odd number.
+    // Add the last '1' to the answer string to make the binary number odd.
+    ans += '1'
 
-    return '1'.repeat(onesCount - 1) + '0'.repeat(binaryString.length - onesCount) + '1';
-
-}
+    // Return the maximum odd binary number.
+    return ans
+};
