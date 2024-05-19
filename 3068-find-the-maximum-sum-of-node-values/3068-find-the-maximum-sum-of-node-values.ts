302402942b211552b2cd1d60fd07f85257774dc5
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-05-19 08:48:47 +0000

    Time: 114 ms (100%), Space: 60.9 MB (100%) - LeetHub

---
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.ts
@@ -0,0 +1,26 @@
+function maximumValueSum(nums: number[], k: number, edges: number[][]): number {
+    let totalSum = 0
+    let count = 0
+    let positiveMin = Infinity
+    let negativeMax = -Infinity
+
+    for (let nodeValue of nums) {
+        let nodeValAfterOperation = nodeValue ^ k
+        totalSum += nodeValue
+        let netChange = nodeValAfterOperation - nodeValue
+
+        if (netChange > 0) {
+            positiveMin = Math.min(positiveMin, netChange)
+            totalSum += netChange
+            count += 1
+        } else {
+            negativeMax = Math.max(negativeMax, netChange)
+        }
+    }
+
+    if (count % 2 === 0) {
+        return totalSum
+    }
+
+    return Math.max(totalSum - positiveMin, totalSum + negativeMax)
+};
