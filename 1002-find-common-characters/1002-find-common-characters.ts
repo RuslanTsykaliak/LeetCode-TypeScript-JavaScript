c9d6e668966900ba7f59a64c2302658a0e8c891b
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-05 10:29:43 +0300

    Time: 65 ms (61.4%), Space: 52 MB (85.96%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.ts
+++ 1002-find-common-characters/1002-find-common-characters.ts
@@ -1,12 +1,21 @@
 function commonChars(words: string[]): string[] {
-    let commonCharacters: string[] = []
-
-    for (let el of words[0]) {
+    return [...words[0]].filter((el) => {
         if (words.every((word) => word.includes(el))) {
-            commonCharacters.push(el)
             words = words.map((word) => word.replace(el, ''))
+            return true
         }
-    }
+        return false
+    })
 
-    return commonCharacters
+    // 75.44%
+    // let commonCharacters: string[] = []
+
+    // for (let el of words[0]) {
+    //     if (words.every((word) => word.includes(el))) {
+    //         commonCharacters.push(el)
+    //         words = words.map((word) => word.replace(el, ''))
+    //     }
+    // }
+
+    // return commonCharacters
 };
