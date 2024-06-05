f34d507defd96ce70368285b831300937e15a427
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-05 10:30:12 +0300

    Time: 64 ms (66.67%), Space: 52.1 MB (82.46%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.ts
+++ 1002-find-common-characters/1002-find-common-characters.ts
@@ -1,13 +1,21 @@
+// This function takes an array of strings as input
 function commonChars(words: string[]): string[] {
+    // The first approach has a performance of 61.40%
+    // It uses the filter method to iterate over each character in the first string of the array
     return [...words[0]].filter((el) => {
+        // If every string in the array includes the current character
         if (words.every((word) => word.includes(el))) {
+            // Replace the first occurrence of the current character in each string with an empty string
             words = words.map((word) => word.replace(el, ''))
+            // Include the current character in the output array
             return true
         }
+        // Exclude the current character from the output array
         return false
     })
 
-    // 75.44%
+    // The second approach has a performance of 75.44%
+    // It uses a for loop to iterate over each character in the first string of the array
     // let commonCharacters: string[] = []
 
     // for (let el of words[0]) {
