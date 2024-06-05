3eb2d43ff4aa0cdb1b866f5007d4ab96511bb829
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-05 10:31:10 +0300

    Time: 56 ms (84.21%), Space: 52 MB (89.47%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.ts
+++ 1002-find-common-characters/1002-find-common-characters.ts
@@ -2,28 +2,28 @@
 function commonChars(words: string[]): string[] {
     // The first approach has a performance of 61.40%
     // It uses the filter method to iterate over each character in the first string of the array
-    return [...words[0]].filter((el) => {
-        // If every string in the array includes the current character
-        if (words.every((word) => word.includes(el))) {
-            // Replace the first occurrence of the current character in each string with an empty string
-            words = words.map((word) => word.replace(el, ''))
-            // Include the current character in the output array
-            return true
-        }
-        // Exclude the current character from the output array
-        return false
-    })
-
-    // The second approach has a performance of 75.44%
-    // It uses a for loop to iterate over each character in the first string of the array
-    // let commonCharacters: string[] = []
-
-    // for (let el of words[0]) {
+    // return [...words[0]].filter((el) => {
+    //     // If every string in the array includes the current character
     //     if (words.every((word) => word.includes(el))) {
-    //         commonCharacters.push(el)
+    //         // Replace the first occurrence of the current character in each string with an empty string
     //         words = words.map((word) => word.replace(el, ''))
+    //         // Include the current character in the output array
+    //         return true
     //     }
-    // }
+    //     // Exclude the current character from the output array
+    //     return false
+    // })
 
-    // return commonCharacters
+    // The second approach has a performance of 87.72%
+    // It uses a for loop to iterate over each character in the first string of the array
+    let commonCharacters: string[] = []
+
+    for (let el of words[0]) {
+        if (words.every((word) => word.includes(el))) {
+            commonCharacters.push(el)
+            words = words.map((word) => word.replace(el, ''))
+        }
+    }
+
+    return commonCharacters
 };
