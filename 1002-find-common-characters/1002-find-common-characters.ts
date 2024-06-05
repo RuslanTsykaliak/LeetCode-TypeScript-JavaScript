db809dd09a549eccab592a4df3194a2553b54a1f
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-05 18:52:12 +0300

    Time: 66 ms (59.65%), Space: 52.1 MB (82.46%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.ts
+++ 1002-find-common-characters/1002-find-common-characters.ts
@@ -1,29 +1,20 @@
-// This function takes an array of strings as input
 function commonChars(words: string[]): string[] {
-    // The first approach has a performance of 61.40%
-    // It uses the filter method to iterate over each character in the first string of the array
-    // return [...words[0]].filter((el) => {
-    //     // If every string in the array includes the current character
-    //     if (words.every((word) => word.includes(el))) {
-    //         // Replace the first occurrence of the current character in each string with an empty string
-    //         words = words.map((word) => word.replace(el, ''))
-    //         // Include the current character in the output array
-    //         return true
-    //     }
-    //     // Exclude the current character from the output array
-    //     return false
-    // })
+    // Initialize an array to store the common characters
+    let commonCharacters: string[] = [];
+    // Create an array of characters from the first string in the input array
+    let firstWordChars: string[] = [...words[0]];
 
-    // The second approach has a performance of 
-    // It uses a for loop to iterate over each character in the first string of the array
-    let commonCharacters: string[] = []
-
-    for (let el of words[0]) {
-        if (words.every((word) => word.includes(el))) {
-            commonCharacters.push(el)
-            words = words.map((word) => word.replace(el, ''))
+    // Iterate over each character in the first string
+    firstWordChars.forEach((char) => {
+        // If every string in the array includes the current character
+        if (words.every((word) => word.includes(char))) {
+            // Add the current character to the array of common characters
+            commonCharacters.push(char);
+            // Replace the first occurrence of the current character in each string with an empty string
+            words = words.map((word) => word.replace(char, ''));
         }
-    }
+    });
 
-    return commonCharacters
+    // Return the array of common characters
+    return commonCharacters;
 };
