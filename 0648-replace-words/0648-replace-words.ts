64f614903880479db125c59579dbf4416f4d5f0a
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-07 19:18:08 +0300

    Time: 1108 ms (20%), Space: 62.7 MB (60%) - LeetHub

--- 0648-replace-words/0648-replace-words.ts
+++ 0648-replace-words/0648-replace-words.ts
@@ -1,21 +1,28 @@
 function replaceWords(dictionary: string[], sentence: string): string {
+    // Convert the dictionary array to a set for faster lookup
     let setDictionary = new Set(dictionary);
+    // Split the sentence into words
     let words = sentence.split(' ');
 
+    // Iterate over each word in the sentence
     for (let i = 0; i < words.length; i++) {
         let word = words[i];
+        // Initialize the shortest root as the word itself
         let shortestRoot = word;
-
+        // Check each substring of the word starting from the first character
         for (let j = 1; j <= word.length; j++) {
-            let root = word.slice(0, j);
+            let root = word.slice(0, j)
+            // If the root exists in the dictionary and is shorter than the current shortest root, update the shortest root
             if (setDictionary.has(root) && root.length < shortestRoot.length) {
                 shortestRoot = root;
             }
         }
-        words[i] = shortestRoot
+        // Replace the word in the sentence with its shortest root
+        words[i] = shortestRoot;
     }
+    // Join the words back into a sentence
+    sentence = words.join(' ');
 
-    sentence = words.join(' ')
-
-    return sentence
+    // Return the modified sentence
+    return sentence;
 };
