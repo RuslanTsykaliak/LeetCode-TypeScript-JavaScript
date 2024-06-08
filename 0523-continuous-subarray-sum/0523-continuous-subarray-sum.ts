8417f01a904f7c8b5f476fbc18d4cfdb4a643320
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-08 22:32:50 +0300

    Time: 106 ms (76.09%), Space: 73.3 MB (80.43%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.ts
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.ts
@@ -1,19 +1,22 @@
 function checkSubarraySum(nums: number[], k: number): boolean {
-    const map: Map<number, number> = new Map();
-    let sum: number = 0;
-    map.set(0, -1);
-    for (let i = 0; i < nums.length; i++) {
+    // good = length at least two, sum is a multiple of k
+    let sum = 0;
+    let remainderMap = new Map();
+    remainderMap.set(0, -1)
+    for(let i = 0; i < nums.length; i++){
         sum += nums[i];
-        const remainder = sum % k;
-        // If the remainder already exists then we've found the subset
-        if (map.has(remainder)) {
-            const length: number = i - map.get(remainder);
-            if (length >= 2) {
+        let r = sum % k;
+
+        if(remainderMap.has(r)){
+            let l = i - remainderMap.get(r);
+            if(l >= 2){
                 return true;
             }
-        } else {
-            map.set(remainder, i);
+        } else{
+            remainderMap.set(r, i);
         }
     }
+
     return false;
+
 };
