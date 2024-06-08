a3c7a98ffac2e2a316d2c7b05564906889e92e68
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-08 22:44:36 +0300

    Time: 91 ms (100%), Space: 74.5 MB (50%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.ts
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.ts
@@ -1,15 +1,25 @@
 function checkSubarraySum(nums: number[], k: number): boolean {
-  nums[0] %= k;
+    // Modulo operation on the first element of the array
+    nums[0] %= k;
 
-  const set = new Set([0]);
-  for (let i = 1; i < nums.length; i++) {
-    nums[i] += nums[i - 1];
-    nums[i] %= k;
+    // Initialize a set to store the cumulative sum of elements modulo k
+    const set = new Set([0]);
 
-    if (set.has(nums[i])) return true;
+    // Iterate over the array starting from the second element
+    for (let i = 1; i < nums.length; i++) {
+        // Update the current element to be the cumulative sum of the current and previous element
+        nums[i] += nums[i - 1];
+        // Perform modulo operation on the updated element
+        nums[i] %= k;
 
-    set.add(nums[i - 1]);
-  }
+        // If the current cumulative sum modulo k is already in the set, it means that there is a subarray
+        // whose sum is a multiple of k, so we return true
+        if (set.has(nums[i])) return true;
 
-  return false;
-}
+        // Add the previous cumulative sum modulo k to the set
+        set.add(nums[i - 1]);
+    }
+
+    // If we have checked all elements and found no subarray whose sum is a multiple of k, we return false
+    return false;
+};
