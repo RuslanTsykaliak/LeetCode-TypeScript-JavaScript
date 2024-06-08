b5017414a7b9e16b27db5d6466104ca417f6bf62
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-08 22:30:58 +0300

    Time: 137 ms (6.52%), Space: 72.6 MB (97.83%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.ts
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.ts
@@ -1,26 +1,19 @@
 function checkSubarraySum(nums: number[], k: number): boolean {
-    // Initialize the cumulative sum
-    let sum = 0;
-    // Initialize a map to store the cumulative sum and its corresponding index
-    let map = new Map([[0, -1]]);
-
-    // Iterate over the array
+    const map: Map<number, number> = new Map();
+    let sum: number = 0;
+    map.set(0, -1);
     for (let i = 0; i < nums.length; i++) {
-        // Update the cumulative sum
         sum += nums[i];
-        // If k is not zero, update the cumulative sum to be its remainder when divided by k
-        if (k != 0) sum = sum % k;
-        // if the current cumulative sum is already in the map, it means that there is a subarray
-        // whose sum is a multiple of k, so we return true
-        if (map.has(sum)) {
-            // The subarray should have at least two elements, so we check if the current index
-            // is greater than the index where the same cumulative sum was found plus one
-            if (i - map.get(sum) > 1) return true;
+        const remainder = sum % k;
+        // If the remainder already exists then we've found the subset
+        if (map.has(remainder)) {
+            const length: number = i - map.get(remainder);
+            if (length >= 2) {
+                return true;
+            }
         } else {
-            // If the current cumulative sum is not in the map, we add it to the map
-            map.set(sum, i);
+            map.set(remainder, i);
         }
     }
-    // If we have checked all elements and found no subarray whose is a multiple of k, we return false
     return false;
 };
