dfdcdccf31742d0a441332a2b87a4881f618a861
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-11 14:10:20 +0300

    Time: 59 ms (61.02%), Space: 51.9 MB (47.46%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.ts
@@ -0,0 +1,16 @@
+function relativeSortArray(arr1: number[], arr2: number[]): number[] {
+    // Create a sortOrderMap that assigns an index to each element in arr2.
+    // This index will represent the sort order for the elements in arr1.
+    const sortOrderMap = Object.fromEntries(arr2.map((num, index) => [num, index]));
+
+    // Sort arr1 by comparing elements based on their sortOrderMap values.
+    // If an element is not found in sortOrderMap (i.e., it's not in arr2),
+    // it will be assigned a sort order that places it at the end of arr1,
+    // sorted in ascending order.
+    return arr1.sort((a, b) =>
+        // If 'a' is in arr2, use its sortOrderMap index, otherwise use
+        // arr2.length + a to ensure it's placed at the end and sorted by value.
+        (sortOrderMap[a] ?? arr2.length + a) -
+        // Apply the same logic for 'b'.
+        (sortOrderMap[b] ?? arr2.length + b));
+};
