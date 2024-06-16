3fef3a12932115cbaf86161af6d64b0bcac01678
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-16 11:50:04 +0300

    Time: 58 ms (60%), Space: 51.7 MB (20%) - LeetHub

--- 0330-patching-array/0330-patching-array.ts
+++ 0330-patching-array/0330-patching-array.ts
@@ -1,8 +1,8 @@
 function minPatches(nums: number[], n: number): number {
-    let x = 1, patches = 0, i = 0;
-    while (x <= n) {
-        if (i < nums.length && nums[i] <= x) x += nums[i++];
-        else x += x, patches++;
+    let x = 1, patches = 0, i = 0;  // Initialize x to 1, patches to 0, and i to 0.
+    while (x <= n) {     // Loop until x exceeds n
+        if (i < nums.length && nums[i] <= x) x += nums[i++]; // If the current number in nums can be used to extend the range add nums[i] to x and move to the next number
+        else x += x, patches++; // If the current number cannot be used or we've run out of numbers in the array
     }
-    return patches;
+    return patches;  // Return the total number of patches added
 };
