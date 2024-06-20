cb9bee2c96b8b59520fa54d9ced28fb321553797
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-20 21:15:25 +0300

    Time: 57 ms (90.6%), Space: 50.4 MB (92.42%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.ts
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.ts
@@ -1,6 +1,6 @@
 function judgeSquareSum(c: number): boolean {
     for (let a = 0; a * a <= c; a++) {
-        const b = Math.sqrt(c - a * a)
+        const b = Math.sqrt(c - a * a);
         if (b === Math.floor(b)) {
             return true;
         }
