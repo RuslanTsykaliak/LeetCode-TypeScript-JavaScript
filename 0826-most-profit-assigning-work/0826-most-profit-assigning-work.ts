43251cb6ec5b8c007e935e4d11123037d25708bb
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-20 20:38:31 +0300

    Time: 98 ms (84.19%), Space: 57.8 MB (67.35%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.ts
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.ts
@@ -1,21 +1,11 @@
 function maxProfitAssignment(difficulty: number[], profit: number[], worker: number[]): number {
-    let res = 0, j = 0, best = 0, temp = [];
-
-    for (let i = 0; i < worker.length; ++i) {
-        temp.push([difficulty[i], profit[i]]);
-    }
-
-    temp.sort((a, b) => a[0] - b[0]);
+    let jobs = [], i = 0, total = 0, best = 0;
+    for (let i = 0; i < difficulty.length; i++) jobs[i] = [difficulty[i], profit[i]];
+    jobs.sort((a, b) => a[0] - b[0]);
     worker.sort((a, b) => a - b);
-
-    for (let work of worker) {
-        while (j < temp.length && work >= temp[j][0]) {
-            best = Math.max(best, temp[j][1]);
-            j++;
-        }
-
-        res += best;
+    for (let ability of worker) {
+        while (i < jobs.length && ability >= jobs[i][0]) best = Math.max(best, jobs[i++][1]);
+        total += best;
     }
-
-    return res;
+    return total;
 };
