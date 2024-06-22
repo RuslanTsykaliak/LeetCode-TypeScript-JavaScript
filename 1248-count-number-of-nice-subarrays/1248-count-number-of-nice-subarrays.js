a81cddd871e6fb81d76b3163988936f8d523eeb1
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-22 12:31:15 +0300

    Time: 58 ms (100%), Space: 63.8 MB (37.25%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.js
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.js
@@ -5,12 +5,12 @@
  */
 var numberOfSubarrays = function (nums, k) {
     let cnt = new Array(nums.length + 1).fill(0);
-    let t = 0, ans = 0;
+    let t = 0, result = 0;
     cnt[0] = 1;
     for (let v of nums) {
         t += v & 1;
-        ans += t - k >= 0 ? cnt[t - k] : 0;
+        result += t - k >= 0 ? cnt[t - k] : 0;
         cnt[t]++;
     }
-    return ans;
+    return result;
 };
