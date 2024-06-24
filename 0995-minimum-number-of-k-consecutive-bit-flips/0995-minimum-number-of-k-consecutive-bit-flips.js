5cdb07109d09ed4a51efe26ae14c9324c6f8434a
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-24 12:08:26 +0300

    Time: 152 ms (11.43%), Space: 67 MB (5.71%) - LeetHub

---
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minKBitFlips = function (nums, k) {
+    let flip = 0, flips = 0, isFlipped = new Array(nums.length).fill(0);
+    for (let i = 0; i < nums.length; i++, flip ^= isFlipped[i - k] || 0) {
+        if ((flip % 2) == nums[i]) {
+            if (i + k > nums.length) return -1;
+            else flips++, flip ^= 1, isFlipped[i] = 1;
+        }
+    }
+    return flips;
+};
