4402796e8aa77b61005ccac1d32ac6298b218924
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-06-29 15:15:30 +0300

    Time: 376 ms (6.25%), Space: 91.9 MB (12.5%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.ts
@@ -0,0 +1,24 @@
+function getAncestors(n: number, edges: number[][]): number[][] {
+    const graph: number[][] = Array.from({ length: n }, () => []);
+    const ancestors: Set<number>[] = Array.from({ length: n }, () => new Set<number>());
+
+    for (const [from, to] of edges) {
+        graph[from].push(to);
+    }
+
+    const dfs = (currentNode: number, ancestor: number) => {
+        for (const child of graph[currentNode]) {
+            if (!ancestors[child].has(ancestor)) {
+                ancestors[child].add(ancestor);
+                dfs(child, ancestor);
+            }
+        }
+    };
+
+    for (let node = 0; node < n; node++) {
+        dfs(node, node);
+    }
+
+    return ancestors.map(ancestorSet => Array.from(ancestorSet).sort((a, b) => a - b));
+
+};
