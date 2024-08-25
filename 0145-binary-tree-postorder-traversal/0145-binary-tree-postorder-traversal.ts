003600894659f81236d8cd2adaea38723fa226c6
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-08-25 09:09:32 +0200

    Time: 62 ms (30.3%), Space: 51.7 MB (21.45%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.ts
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+    if (!root) return []
+    return [...postorderTraversal(root.left), ...postorderTraversal(root.right), root.val]
+};
