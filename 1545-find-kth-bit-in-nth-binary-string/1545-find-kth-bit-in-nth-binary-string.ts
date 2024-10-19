07f81a1a4272951b8abd4707de953d7028f12460
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-10-18 21:34:30 -0400

    Time: 2556 ms (7.69%), Space: 123.8 MB (7.69%) - LeetHub

---
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.ts
@@ -0,0 +1,25 @@
+function findKthBit(n: number, k: number): string {
+    function recursion(str: string, index: number): string {
+        if (!(index <= n)) return str
+
+        const newStr = str + '1' + reverse(invert(str))
+
+        return recursion(newStr, index + 1)
+    }
+
+    const returnNum = recursion('0', 1)
+
+    return returnNum[k - 1]
+};
+
+function invert(s: string): string {
+    return s.split('').map(el => el === '1' ? '0' : '1').join('')
+}
+
+function reverse(s: string): string {
+    let returnString: string = ''
+
+    for (let char of s) returnString = char + returnString
+
+    return returnString
+}
