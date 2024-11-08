90391a0e6d97aab3f7b0357d5ae4a3e026a2850c
Ruslan Tsykaliak <ruslan.tsykaliak@gmail.com>
2024-11-07 20:41:05 -0500

    Time: 5 ms (100%), Space: 76.3 MB (75%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.ts
@@ -0,0 +1,13 @@
+function getMaximumXor(nums: number[], maximumBit: number): number[] {
+    const mask: number = (1 << maximumBit) - 1;
+    const n: number = nums.length;
+    const res: number[] = new Array(n);
+    let curr: number = 0;
+
+    for (let i = 0; i < n; i++) {
+        curr ^= nums[i];
+        res[n - i - 1] = (~curr & mask);
+    }
+
+    return res;
+}
